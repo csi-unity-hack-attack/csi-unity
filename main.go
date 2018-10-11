ca237da2e931170e03da700800213ff3699d6f3a
jicahoo <jicahoo@163.com>
2018-10-11 15:23:08 +0800

    basic framework

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/Murray-LIANG/gounity"
+	"github.com/jicahoo/csi-unity/provider"
+	"github.com/jicahoo/csi-unity/service"
+	"github.com/rexray/gocsi"
 )
 
 func main() {
@@ -17,4 +21,23 @@ func main() {
 	}
 
 	fmt.Println("Hello, World")
+
+	gocsi.Run(
+		context.Background(),
+		service.Name,
+		"A Unity Container Storage Interface (CSI) Plugin",
+		usage,
+		provider.New())
 }
+
+const usage = `
+    X_CSI_UNITY_ENDPOINT
+	Unity management IP address
+ 		
+
+    X_CSI_UNITY_USER
+	Unity user name
+
+    X_CSI_UNITY_PASSWORD
+	Unity password
+`
---
+++ provider/provider.go
@@ -0,0 +1,7 @@
+package provider
+
+import "github.com/rexray/gocsi"
+
+func New() gocsi.StoragePluginProvider {
+	return nil
+}
