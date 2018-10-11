eec2d51a005875aae49b53ac325dec3bda7eb7ed
Jichao <jicahoo@163.com>
2018-10-11 11:29:58 +0800

    First call to gounity

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
-    unity, err := gounity.NewUnity("UnityMgmtIP", "username", "password", true)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Hello, World")
+	unity, err := gounity.NewUnity("10.141.68.198", "admin", "Password123!", true)
+	if err != nil {
+		panic(err)
+	}
+	pools, err := unity.GetPools()
+	fmt.Println("Hello")
+	for _, pool := range pools {
+		fmt.Println(pool.Name)
+	}
+
+	fmt.Println("Hello, World")
 }
