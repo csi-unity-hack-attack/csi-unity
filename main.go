0e46cade1c902bc16ab7696f9a08ba33e7aebabf
jicahoo <jicahoo@163.com>
2018-10-12 03:07:24 +0000

    Add gounity dependency

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"context"
 
 	"github.com/rexray/gocsi"
 
 	"github.com/jicahoo/csi-unity/provider"
 	"github.com/jicahoo/csi-unity/service"
+	"github.com/Murray-LIANG/gounity"
 )
 
 // main is ignored when this package is built as a go plug-in.
 func main() {
+	unity, err := gounity.NewUnity("10.141.68.198", "admin", "Password123!", true)
+	if err != nil {
+		panic(err)
+	}
+	pools, err := unity.GetPools()
+	fmt.Println("Hello")
+	for _, pool := range pools {
+		fmt.Println(pool.Name)
+	}
+	fmt.Println("Hello World")
 	gocsi.Run(
 		context.Background(),
 		service.Name,
