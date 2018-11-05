c82347b8e906cf8c8a23b1182bc5fdb2beff9815
Grace-xh <junglehui@aliyun.com>
2018-11-05 16:08:40 +0800

    create file interface

--- service/file_controller_create_volume.go
+++ service/file_controller_create_volume.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"bytes"
+	"container/list"
 	"context"
 	"errors"
 	"fmt"
@@ -24,6 +25,21 @@ func FileCreateVolume(
 	name := req.GetName()
 	capRange := req.GetCapacityRange()
 	size := capRange.GetRequiredBytes()
+    //create io interface for volume first
+    var interfaceData = map[string]string {
+    	"NasId": "nas_1",
+    	"IPPort": "spa_iom_0_eth0",
+    	"IPAddress": "10.103.76.148",
+    	"Netmask": "255.255.248.0",
+    	"Gateway": "10.103.72.1",
+	}
+
+	_, err := createFileInterface(s.RestEndpoint, interfaceData["NasId"], interfaceData["IPPort"], interfaceData["IPAddress"], interfaceData["Netmask"], interfaceData["Gateway"])
+	if err!=nil {
+		logrus.Error("Error: Failed to create file interface.")
+		return nil, err
+	}
+
 	volAttrMap, _ := createVolumeByRest(s.RestEndpoint, uint64(size), name)
 	vol := &csi.Volume{
 		Id:            volAttrMap["id"],
@@ -185,4 +201,69 @@ func queryShareData(conn RestEndpoint, fsName string) (map[string]string, error)
 		logrus.Info(nfsShareMetaData)
 	}
 	return nfsShareMetaData, err
+}
+
+func queryFileInterface(conn RestEndpoint, nasId string) (*list.List, error){
+	var getFileInterfaceUrl string = "/api/types/fileInterface/instances?fields=id,nasServer"
+    encodedQuery := EncodeUrl(getFileInterfaceUrl)
+    logrus.Debug("Encoded: ", encodedQuery)
+    status, resp := conn.get(encodedQuery)
+    logrus.Debug("Status: ", status)
+    logrus.Debug("Query file interface response: ", resp)
+    jsonParsed, _ := gabs.ParseJSON([]byte(resp))
+    entryCount := jsonParsed.Path("entryCount").Data().(float64)
+    fileInterfaces := list.New();
+
+
+    var err error = nil
+    if int(entryCount) == 0 {
+    	logrus.Info("Info: the num of file interface entries is 0")
+    	//err = errors.New(fmt.Sprintf("Error: no file interface created for nas server:%s ", nasId))
+	}else{
+		children, _ := jsonParsed.S("entries").Children()
+		for _, child := range children{
+			if child.Path("content").Path("nasServer").Data().(string) == nasId {
+				fileInterfaceData := make(map[string]string)
+				fileInterfaceData["id"] = child.Path("content").Path("id").Data().(string)
+				fileInterfaces.PushBack(fileInterfaceData);
+				logrus.Info(fileInterfaceData)
+			}
+		}
+		logrus.Info(fileInterfaces)
+	}
+    return fileInterfaces, err
+}
+
+func createFileInterface(conn RestEndpoint, nasId string, ipPort string, ipAddr string, netMask string, gateway string)(map[string]string, error) {
+	var err error = nil
+	fileInterfaceData := make(map[string]string)
+
+	fileInterfaces, _ := queryFileInterface(conn, nasId)
+	if fileInterfaces.Len() == 0 {
+		logrus.Info("Info: file interface is not created for nas server %s", nasId)
+		// start to create interface
+		var createFileInterfaceUrl string = "/api/types/fileInterface/instances"
+		//body: {"nasServer": {"id":"nas_1"}, "ipPort": {"id":"spa_iom_0_eth0"}, "ipAddress": "10.103.76.143","netmask":"255.255.248.0", "gateway":"10.103.72.1"}
+		var createFileInterfaceBody string = fmt.Sprintf(`{"nasServer": {"id":"%s"}, "ipPort": {"id":"%s"}, "ipAddress":"%s", "netmask":"%s", "gateway":"%s"}`, nasId, ipPort, ipAddr, netMask, gateway)
+		status, resp := conn.post(createFileInterfaceUrl, createFileInterfaceBody)
+		logrus.Debug("Status: ", status)
+		logrus.Debug("Create file interface response: ", resp)
+		jsonParsed, _ := gabs.ParseJSON([]byte(resp))
+		content := jsonParsed.Path("content").Data().(float64)
+
+		if int(content) != 1 {
+			logrus.Error("Error: failed to create file interface for nas server %s", nasId)
+			err = errors.New(fmt.Sprintf("Error: failed to create file interface for nas server:%s", nasId))
+		}else{
+			children, _ :=jsonParsed.S("content").Children()
+			for _, child := range children{
+				fileInterfaceData["id"] =  child.Path("id").Data().(string)
+			}
+		}
+
+	} else {
+		logrus.Info("File interface(s) are already created for nas server %s", nasId)
+	}
+
+	return nil, err
 }
